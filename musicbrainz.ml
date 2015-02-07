8ee40b91eada4273ddc7e493906f45d2ffbcb8c3
Benjamin <benjamin.farinier@ens-lyon.fr>
2015-02-07 20:15:58 +0100

    musicbrainz is doing now real stuff

--- musicbrainz.ml
+++ musicbrainz.ml
@@ -1,13 +1,15 @@
+open Lwt_ops
+
 type artist_tags = [`Area|`Beginarea|`Endarea|`Arid|`Artist|`Artistaccent|`Alias|`Begin|`Comment|`Country|`End|`Ended|`Gender|`Ipi|`Sortname|`Tag|`Type]
 type freedb_tags = [`Artist|`Title|`Discid|`Cat|`Year|`Tracks]
 type tag_tags    = [`Tag]
 
 type tags = [artist_tags|freedb_tags|tag_tags]
 
-type _ t =
-  | Artist : artist_tags t
-  | Freedb : freedb_tags t
-  | Tag    : tag_tags t
+type _ searches =
+  | Artist : artist_tags searches
+  | Freedb : freedb_tags searches
+  | Tag    : tag_tags searches
 
 let tags_to_string : [< tags] -> string = function
   | `Area -> "area"
@@ -33,17 +35,74 @@ let tags_to_string : [< tags] -> string = function
   | `Year -> "year"
   | `Tracks -> "tracks"
 
-let t_to_string : type a. a t -> string = function
+let searches_to_string : type a. a searches -> string = function
   | Artist -> "artist"
   | Freedb -> "freedb"
   | Tag -> "tag"
 
-let make_request (index: 'a t) (tag: 'a) ?(limit=25) ?(offset=0) str : string =
+let make_request (index: 'a searches) (tag: 'a) ?(limit=25) ?(offset=0) str : string =
   Printf.sprintf "http://musicbrainz.org/ws/2/%s/?query=%s:%s%s%s"
-    (t_to_string index) (tags_to_string tag) str
+    (searches_to_string index) (tags_to_string tag) str
     (if (limit <> 25 && limit >= 1 && limit <= 100)
      then (Printf.sprintf "&limit=%i" limit)
      else "")
     (if (offset > 0)
      then (Printf.sprintf "&offset=%i" offset)
      else "")    
+
+let get_strings (url: string) : string list Lwt.t =
+  let rec aux acc stream =
+    Ocsigen_stream.next stream >>= function
+    | Ocsigen_stream.Finished None -> Lwt.return (List.rev acc)
+    | Ocsigen_stream.Finished (Some stream) -> aux acc stream
+    | Ocsigen_stream.Cont (x, stream) -> aux (x::acc) stream
+  in
+  Ocsigen_http_client.get_url url >>= fun frame ->
+  match frame.Ocsigen_http_frame.frame_content with
+  | None -> raise Not_found
+  | Some s -> aux [] (Ocsigen_stream.get s)
+
+let iter_strings (strings: string list) : unit -> int =
+  let length, string =
+    match strings with
+    | [] -> (ref 0, ref "")
+    | s::_ -> (ref (String.length s), ref s)
+  in
+  let strings = ref strings in
+  let pos = ref 0 in
+  let rec aux = fun () ->
+    if (!pos) < (!length) then
+      let char = String.get (!string) (!pos) in
+      (incr pos; int_of_char char)
+    else match (!strings) with
+      | [] -> raise End_of_file
+      | s::l -> (
+          strings := l;
+          string := s;
+          length := String.length s;
+          pos := 0;
+          aux ()
+        )
+  in aux
+
+
+module Xml_tree = struct
+
+  type name = string * string
+  type attribute = name * string
+  type tag = name * attribute list
+
+  type t = Element of Xmlm.tag * t list | Data of string
+
+  let tree_of_strings (strings: string list) : t =
+    let input = Xmlm.make_input (`Fun (iter_strings strings)) in
+    let el tag childs = Element (tag, childs)  in
+    let data d = Data d in
+    snd (Xmlm.input_doc_tree ~el ~data input)
+
+  let get_xml (index: 'a searches) (tag: 'a) ?(limit=25) ?(offset=0) str : t Lwt.t =
+    make_request index tag ~limit ~offset str
+    |> get_strings
+    >|= tree_of_strings
+
+end
--- musicbrainz.mli
+++ musicbrainz.mli
@@ -3,11 +3,22 @@ type freedb_tags = [`Artist|`Cat|`Discid|`Title|`Tracks|`Year]
 type tag_tags    = [`Tag]
 
 type tags = [artist_tags|freedb_tags|tag_tags]
-type _ t =
-  | Artist : artist_tags t
-  | Freedb : freedb_tags t
-  | Tag    : tag_tags t
+type _ searches =
+  | Artist : artist_tags searches
+  | Freedb : freedb_tags searches
+  | Tag    : tag_tags searches
 
-val tags_to_string : [< tags ] -> string
-val t_to_string : 'a t -> string
-val make_request : ([< tags ] as 'a) t -> 'a -> ?limit:int -> ?offset:int -> string -> string
+val tags_to_string : tags -> string
+val searches_to_string : _ searches -> string
+
+module Xml_tree : sig
+
+  type name = string * string
+  type attribute = name * string
+  type tag = name * attribute list
+
+  type t = Element of tag * t list | Data of string
+
+  val get_xml : 'a searches -> ([< tags ] as 'a) -> ?limit:int -> ?offset:int -> string -> t Lwt.t
+
+end
