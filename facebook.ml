c636451be3e2bc60b413c74c1137ad8dae711f10
Benjamin <benjamin.farinier@ens-lyon.fr>
2015-02-11 18:31:22 +0100

    derp

--- facebook.ml
+++ facebook.ml
@@ -1,8 +1,22 @@
 open Lwt_ops
 
-let client_id = "401808273311928"
-let client_secret = "9c926f3785998ebd4c43a37c29bff882"
-let redirect_uri = "http://project.hotbeverage.org/"
+let get_string (url: string) : string Lwt.t =
+  let buff = Buffer.create 256 in
+  let rec aux stream =
+    Ocsigen_stream.next stream >>= function
+    | Ocsigen_stream.Finished None -> Lwt.return (Buffer.contents buff)
+    | Ocsigen_stream.Finished (Some stream) -> aux stream
+    | Ocsigen_stream.Cont (x, stream) -> Buffer.add_string buff x; aux stream
+  in
+  Ocsigen_http_client.get_url url >>= fun frame ->
+  match frame.Ocsigen_http_frame.frame_content with
+  | None -> raise Not_found
+  | Some s ->
+    let l = aux (Ocsigen_stream.get s) in
+    lwt _ = Ocsigen_stream.finalize s `Success in l
+
+
+
 
 let log_people_in ~client_id ~redirect_uri =
   Printf.sprintf "https://www.facebook.com/dialog/oauth?%s"
@@ -19,6 +33,11 @@ let confirme_identity ~client_id ~client_secret ~redirect_uri ~code =
         "redirect_uri", redirect_uri;
         "code", code])
 
+let confirme_identity ~client_id ~client_secret ~redirect_uri ~code =
+  confirme_identity ~client_id ~client_secret ~redirect_uri ~code
+  |> get_string
+
+
 let client_access_token ~client_id ~client_secret =
   Printf.sprintf "https://graph.facebook.com/oauth/access_token?%s"
     (Ocsigen_lib.Url.make_encoded_parameters
@@ -32,31 +51,7 @@ let inspect_access_token ~input_token ~access_token =
        ["input_token", input_token;
         "access_token", access_token])
 
-let fast_log_people_in =
-  log_people_in ~client_id ~redirect_uri
 
-let fast_confirme_identity =
-  confirme_identity ~client_id ~client_secret ~redirect_uri
-
-let fast_get_access_token =
-  client_access_token ~client_id ~client_secret
-
-
-
-let get_string (url: string) : string Lwt.t =
-  let buff = Buffer.create 256 in
-  let rec aux stream =
-    Ocsigen_stream.next stream >>= function
-    | Ocsigen_stream.Finished None -> Lwt.return (Buffer.contents buff)
-    | Ocsigen_stream.Finished (Some stream) -> aux stream
-    | Ocsigen_stream.Cont (x, stream) -> Buffer.add_string buff x; aux stream
-  in
-  Ocsigen_http_client.get_url url >>= fun frame ->
-  match frame.Ocsigen_http_frame.frame_content with
-  | None -> raise Not_found
-  | Some s ->
-    let l = aux (Ocsigen_stream.get s) in
-    lwt _ = Ocsigen_stream.finalize s `Success in l
 
 
 let get_userid (url: string) : string option Lwt.t =
--- wdmproject_config.eliom
+++ wdmproject_config.eliom
@@ -22,3 +22,6 @@ let _ = Eliom_config.parse_config [avatars]
 
 let db_name = "library_db"
 let () = Freebase.set_key "AIzaSyCDJwSCMuHNEe5Pi0nrQyK_K5AoNF9SRS0"
+
+let client_id = "0123456789"
+let client_secret = "0123456789"
